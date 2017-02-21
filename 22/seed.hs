ca6cc5faaee05fd888183b3f0b2d6300206b8bfe
ninepints <dev@ninepints.co>
2017-02-20 22:08:32 -0800

    Finished exercise 22

---
+++ 22/seed.hs
@@ -0,0 +1,14 @@
+import Data.Time.Clock.POSIX (getPOSIXTime)
+
+import System.Random.Mersenne (next, seedGen)
+
+
+main :: IO ()
+main = do
+    -- This would be harder (but still feasible) to guess
+    -- if we didn't round to the nearest second
+    now <- fmap round getPOSIXTime
+    putStrLn $ "The top secret seed is " ++ show now
+    let gen = seedGen now
+        output = fst $ next gen
+    putStrLn $ "The first 32 bits of output are " ++ show output
