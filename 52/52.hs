f21e93f1d2d40aef5a565622e2126998b883e3a2
ninepints <dev@ninepints.co>
2017-06-04 15:02:30 -0700

    Account for length padding in exercise 52
    Messages with different lengths should not be considered colliding.
    Also tried to make the code a little more comprehensible

--- 52/52.hs
+++ 52/52.hs
@@ -1,129 +1,83 @@
-import Control.Applicative (liftA2)
+import Control.Monad (guard)
 import qualified Data.ByteString as B
 import Data.Function (on)
-import Data.List (groupBy, intercalate)
+import Data.List (intercalate)
 
-import Crypto.Cipher.AES (AES128)
-import Crypto.Cipher.Types (blockSize, cipherKeySize, cipherInit, ecbEncrypt,
-                            KeySizeSpecifier(..))
-import Crypto.Error (CryptoFailable(..))
-
-import ByteFormat (integerToBytes)
-import Padding (constantPadLeft)
 import Util (powerList, pairs)
-
-
-type MDHash = B.ByteString -> B.ByteString -> B.ByteString
-
-data Collision a = Collision {
-    getAttempts :: a,
-    getInput1 :: B.ByteString,
-    getInput2 :: B.ByteString
-} deriving Show
-
-type CollidingBlock = Collision Integer
-type CollidingPair = Collision (Integer, Integer)
-
-bs :: Integer
-bs = fromIntegral $ blockSize (undefined :: AES128)
-
-
-makeHash :: Integer -> Integer -> (Integer, MDHash)
-makeHash size offset = (size, f)
-    where
-        size' = fromIntegral size
-        offset' = fromIntegral offset
-        f state bytes
-            | B.length state /= size' = error "Bad state size"
-            | B.length bytes `mod` bs' /= 0 = error "Bad input size"
-            | otherwise = B.take size' $ B.drop offset' cipherOutput
-            where
-                cipher :: AES128
-                KeySizeFixed ks = cipherKeySize cipher
-                ks' = fromIntegral ks
-                bs' = fromIntegral bs
-                CryptoPassed cipher = cipherInit $ constantPadLeft ks' 0 state
-                cipherOutput = ecbEncrypt cipher bytes
+import WeakHash (makeWeakHash, findCollision1, getAttempts,
+                 getBlockA, getBlockB, getState, HashFunc, Collision)
 
 
 main :: IO ()
 main = do
-    let f = makeHash 1 0
-        g = makeHash 2 1
+    let f = makeWeakHash 1 0
+        g = makeWeakHash 2 1
 
-        fBlocks = getCollidingBlocks f
-        gBlocks = getCollidingBlocks g
-        hPairs = getCollidingPairs f g
-
-        -- How many consecutive colliding blocks do we need in
-        -- order to generate some number of colliding pairs?
-        pairsFromBlocks 1 = 1
-        pairsFromBlocks b = sum [1..(2^b)-1] + pairsFromBlocks (b-1)
-        blocksReqForPairs p = head $ filter ((>= p) . pairsFromBlocks) [1..]
-
+        -- How many consecutive colliding block pairs do we need in
+        -- order to generate some number of colliding message pairs?
+        pairsFromBlocks b = sum [1..(2^b)-1]
+        blocksReqForMsgs p = head $ filter ((>= p) . pairsFromBlocks) [1..]
         nthElem l = (!!) l . flip (-) 1
-        ns = [100, 1000, 10000, 100000]
 
-        fCounts = map (getAttempts . nthElem fBlocks . blocksReqForPairs) ns
-        gCounts = map (getAttempts . nthElem gBlocks . blocksReqForPairs) ns
-        hCounts = map (getAttempts . nthElem hPairs) ns
+        attemptsReqForMsgs hash =
+            map (nthElem (attemptsReqForBlocks hash) . blocksReqForMsgs) [1..]
 
-    putStrLn "Calls to f required for 100/1k/10k/100k pairs:"
-    putStrLn $ intercalate "\n" $ map show fCounts
-    putStrLn "Calls to g required for 100/1k/10k/100k pairs:"
-    putStrLn $ intercalate "\n" $ map show gCounts
-    putStrLn "Calls to (f,g) required for 100/1k/10k/100k pairs in f || g:"
-    putStrLn $ intercalate "\n" $ map show hCounts
+    putStrLn $ "Calls to f compression func required for 1k/10k/100k/1m " ++
+        "pairs in f:"
+    putStrLn $ intercalate "\n" $
+        map (show . nthElem (attemptsReqForMsgs f) . (10 ^)) [4..7]
+
+    putStrLn $ "Calls to g compression func required for 1k/10k/100k/1m " ++
+        "pairs in g:"
+    putStrLn $ intercalate "\n" $
+        map (show . nthElem (attemptsReqForMsgs g) . (10 ^)) [4..7]
+
+    putStrLn $ "Calls to (f compression func, g) required for pairs in f || g:"
+    putStrLn $ intercalate "\n" $
+        map (show . nthElem (attemptsReqForMsgsComposite f g)) [1..8]
 
 
-getCollidingBlocks :: (Integer, MDHash) -> [CollidingBlock]
-getCollidingBlocks (fs, f) = _getCollidingBlocks f initState 0
-    where initState = B.replicate (fromIntegral fs) 0
-
-
-getCollidingPairs :: (Integer, MDHash) -> (Integer, MDHash) -> [CollidingPair]
-getCollidingPairs (fs, f) (gs, g) = result
+findCollidingBlocks :: (Integer, HashFunc) -> (B.ByteString, [Collision])
+findCollidingBlocks (stateSize, hash) = (initState, iterate next initCollision)
     where
-        fInitState = B.replicate (fromIntegral fs) 0
-        gInitState = B.replicate (fromIntegral gs) 0
+        initState = B.replicate (fromIntegral stateSize) 0
+        initCollision = findCollision1 hash initState
+        next collision = findCollision1 hash $ getState collision
 
-        fBlocks = _getCollidingBlocks f fInitState 0
 
-        -- Groups of colliding bytestrings under f
-        -- The first value of each tuple is equal across each group and
-        -- is the number of f invocations it took to find that group
-        fBlocksGrouped :: [[(Integer, B.ByteString)]]
-        fBlocksGrouped = groupBy ((==) `on` fst) $ do
+-- How many compression function invocations are required to generate
+-- the given number of consecutive colliding block pairs? First element
+-- is the invocations for one pair, second for two pairs, etc.
+attemptsReqForBlocks :: (Integer, HashFunc) -> [Integer]
+attemptsReqForBlocks = scanl1 (+) . map getAttempts . snd . findCollidingBlocks
+
+
+attemptsReqForMsgsComposite ::
+    (Integer, HashFunc) -> (Integer, HashFunc) -> [(Integer, Integer)]
+attemptsReqForMsgsComposite f (stateSizeG, hashG) = result
+    where
+        (_, collisions) = findCollidingBlocks f
+        initStateG = B.replicate (fromIntegral stateSizeG) 0
+
+        -- Groups of colliding messages under f, plus compression
+        -- function invocations required to find each group
+        msgsGrouped :: [(Integer, [B.ByteString])]
+        msgsGrouped = do
             i <- [0..]
-            bools <- powerList [False, True] (fromIntegral i + 1)
-            let collisionsThroughI = take (i+1) fBlocks
-                chooseBlock bool = if bool then getInput2 else getInput1
-                blocks = zipWith chooseBlock bools collisionsThroughI
-            return (getAttempts $ fBlocks !! i, B.concat blocks)
+            let collisionsThruI = take (i+1) collisions
+                attemptsThruI = sum $ map getAttempts collisionsThruI
+                messages = do
+                    bools <- powerList [False, True] (fromIntegral i + 1)
+                    let ab bool = if bool then getBlockB else getBlockA
+                    return $ B.concat $ zipWith ab bools collisionsThruI
+            return (attemptsThruI, messages)
 
-        -- Pairs of colliding bytestrings under f
-        fBlocksPaired = fBlocksGrouped >>= pairs
+        -- Pairs of colliding messages under f
+        msgsPaired :: [(Integer, [(B.ByteString, B.ByteString)])]
+        msgsPaired = map (fmap pairs) msgsGrouped
 
-        collidesUnderG = (==) `on` g gInitState
-
-        zipPairs ((fAttempts, input1), (fAttempts', input2)) i
-            | fAttempts /= fAttempts' = error "Your code is off somewhere"
-            | otherwise = Collision (fAttempts, i) input1 input2
-
-        result = filter (liftA2 collidesUnderG getInput1 getInput2) $
-            zipWith zipPairs fBlocksPaired [1..]
-
-
-_getCollidingBlocks :: MDHash -> B.ByteString -> Integer -> [CollidingBlock]
-_getCollidingBlocks f state nAttempts = collision : collisions
-    where
-        prepInteger = constantPadLeft bs 0 . integerToBytes
-        getNextState = f state . prepInteger
-        state' = getNextState 0
-        possibleMatches = [(i, getNextState i) | i <- [1..]]
-        filteredMatches = filter ((==) state' . snd) possibleMatches
-        (addlAttempts, _) = head filteredMatches
-        nAttempts' = nAttempts + 1 + addlAttempts
-        collision = Collision nAttempts'
-            (prepInteger 0) (prepInteger addlAttempts)
-        collisions = _getCollidingBlocks f state' nAttempts'
+        result = do
+            (attemptsF, msgPairs) <- msgsPaired
+            (attemptsG, (msgA, msgB)) <- zip [1..] msgPairs
+            guard $ ((==) `on` hashG initStateG) msgA msgB
+            return (attemptsF, attemptsG)
---
+++ WeakHash.hs
@@ -0,0 +1,110 @@
+module WeakHash (
+    blockSize,
+    makeWeakHash,
+    findCollision1,
+    findCollision2,
+    HashFunc,
+    Collision(..)
+) where
+
+import qualified Data.ByteString as B
+import qualified Data.Map.Strict as Map
+
+import Crypto.Cipher.AES (AES128)
+import qualified Crypto.Cipher.Types as CT
+import Crypto.Error (CryptoFailable(..))
+
+import ByteFormat (integerToBytes)
+import Padding (constantPadLeft)
+
+
+type HashFunc = B.ByteString -> B.ByteString -> B.ByteString
+
+data Collision = Collision {
+    getAttempts :: Integer,
+    getBlockA :: B.ByteString,
+    getBlockB :: B.ByteString,
+    getState :: B.ByteString
+} deriving Show
+
+
+blockSize :: Integer
+blockSize = fromIntegral $ CT.blockSize (undefined :: AES128)
+
+
+makeWeakHash :: Integer -> Integer -> (Integer, HashFunc)
+makeWeakHash size offset = (size, f)
+    where
+        size' = fromIntegral size
+        offset' = fromIntegral offset
+        f state bytes
+            | B.length state /= size' = error "Bad state size"
+            | B.length bytes `mod` blockSize' /= 0 = error "Bad input size"
+            | B.null bytes = state
+            | otherwise = f state' bytesTail
+            where
+                cipher :: AES128
+                CT.KeySizeFixed keySize = CT.cipherKeySize cipher
+                keySize' = fromIntegral keySize
+                blockSize' = fromIntegral blockSize
+                key = constantPadLeft keySize' 0 state
+                CryptoPassed cipher = CT.cipherInit key
+
+                bytesHead = B.take blockSize' bytes
+                bytesTail = B.drop blockSize' bytes
+
+                cipherOutput = CT.ecbEncrypt cipher bytesHead
+                state' = B.take size' $ B.drop offset' cipherOutput
+
+
+toBlock :: Integer -> B.ByteString
+toBlock = constantPadLeft blockSize 0 . integerToBytes
+
+maxBlock :: Integer
+maxBlock = 2 ^ (blockSize * 8) - 1
+
+allBlocks :: [B.ByteString]
+allBlocks = map toBlock [0..maxBlock]
+
+
+findCollision1 :: HashFunc -> B.ByteString -> Collision
+findCollision1 f state = _find1 f 0 state Map.empty allBlocks
+
+
+findCollision2 :: HashFunc -> B.ByteString -> B.ByteString -> Collision
+findCollision2 f stateA stateB = if stateA == stateB
+    then _find1 f 0 stateA Map.empty allBlocks
+    else _find2 f 0 False stateA Map.empty allBlocks stateB Map.empty allBlocks
+
+
+_find1 :: HashFunc -> Integer ->
+    B.ByteString -> Map.Map B.ByteString B.ByteString -> [B.ByteString] ->
+    Collision
+_find1 f attempts state outputMap inputs = collision
+    where
+        (inputA : inputs') = inputs
+        output = f state inputA
+        outputMap' = Map.insert output inputA outputMap
+        attempts' = attempts + 1
+
+        collision = case Map.lookup output outputMap of
+            Just inputB -> Collision attempts' inputA inputB output
+            Nothing -> _find1 f attempts' state outputMap' inputs'
+
+
+_find2 :: HashFunc -> Integer -> Bool ->
+    B.ByteString -> Map.Map B.ByteString B.ByteString -> [B.ByteString] ->
+    B.ByteString -> Map.Map B.ByteString B.ByteString -> [B.ByteString] ->
+    Collision
+_find2 f attempts swapped stateA mapA inputsA stateB mapB inputsB = c
+    where
+        (inputA : inputsA') = inputsA
+        outputA = f stateA inputA
+        mapA' = Map.insert outputA inputA mapA
+        attempts' = attempts + 1
+
+        c = case (Map.lookup outputA mapB, swapped) of
+            (Just inputB, False) -> Collision attempts' inputA inputB outputA
+            (Just inputB, True) -> Collision attempts' inputB inputA outputA
+            (Nothing, _) -> _find2 f attempts' (not swapped)
+                stateB mapB inputsB stateA mapA' inputsA'
